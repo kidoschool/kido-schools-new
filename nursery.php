40523820c16e83c500fe5c52b7f11dbdbfd13177
kidoschool <fauzan.falke@kido.school>
2022-02-28 17:59:47 +0530

    updated search page

--- nursery.php
+++ nursery.php
@@ -1,10 +1,10 @@
 <?php include 'header.php';?>
-<section class="nursery-head-title py-4">
+<section class="nursery-head-title pt-5">
     <div class="container">
         <div class="row">
             <div class="col-lg-8">
                 <div class="title">
-                    <h2>Bandra International Preschool & Day Care</h2>
+                    <h1>Bandra International Preschool & Day Care</h1>
                     <p>India, Maharashtra, Mumbai</p>
                     <div class="rating d-flex justify-content-between">
                     <div class="google-rating">
@@ -37,7 +37,7 @@
         </div>
     </div>
 </section>
-<section class="nursery-gallery-grid pb-5">
+<section class="nursery-gallery-grid pt-5">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 padding-0">
@@ -60,6 +60,54 @@
             <div class="col-lg-3 padding-0">
                 <img src="https://kido.school/wp-content/uploads/2020/11/Facebook-Kido-London-Wandsworth-Classroom_01.jpg" class="img-fluid" alt="...">
                 <img src="https://kido.school/wp-content/uploads/2020/11/Facebook-Kido-London-Wandsworth-Classroom_01.jpg" class="img-fluid mt-2" alt="..."> 
+            </div>
+        </div>
+    </div>
+</section>
+<section class="nursery-data pt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="nursery-desc">
+                    <h2>Welcome to Kido Bandra, Mumbai</h2>
+                    <p>Kido International Preschool and Day Care (Formerly Safari Kid) is a wonderful 3500 sq ft space in the heart of Bandra, off Carter Road and near Khar & Santa Cruz.</p>
+                    <p>It’s a palace of imagination – complete with a large play area, themed spaces, and an indoor stage for drama, music, and dance. Between the reading nooks and creative corners, our international preschool is also furnished with science and tech bays – so children can learn whilst playing with a diverse range of advanced equipment.</p>
+                    <p>Each preschool classroom is bright, warm and spacious, keeping your child engaged in a program that’s designed just for them.</p>
+                    <p>Our programs are for children between the ages of 8 months and 5 years, including preschool extended days and full days.</p>
+                    <p>Give us a call and we can chat about our Bandra Preschool in more detail, or arrange a tour so you can see what the place is like!</p>
+                </div>
+                <div class="school-details mt-4">
+                    <h2>School details</h2>
+                    <p>Curriculum <span class="font-weight-bold">EYFS, Kïdo</span></p>
+                    <p>Hours <span class="font-weight-bold">7:30 am - 6:30 pm</span></p>
+                    <p>Schedule <span class="font-weight-bold">Part time, Full time, 51 weeks/year</span></p>
+                    <p>Nutrition <span class="font-weight-bold">Breakfast, Lunch, Snacks</span></p>
+                    <p>Outdoor Space <span class="font-weight-bold">Yes</span></p>
+                </div>
+            </div>
+            <div class="col-lg-4 offset-lg-1">
+                <div class="nursery-contact">
+                    <div class="nur-cont border p-4 shadow text-center bg-white">
+                        <h3 class="my-4">Contact Bandra International Preschool & Day Care</h3>
+                        <div class="mt-3">
+                        <button class="btn btn-primary btn-sm" type="button"><i class="fas fa-envelope text-white pr-3"></i>Send Message</button>
+                        <button class="btn-sm btn btn-primary" type="button"><i class="fas fa-phone text-white pr-3"></i>Give us a call</button>
+                        <button class="btn-sm btn btn-primary" type="button"><i class="fab fa-facebook-f text-white pr-3"></i>Facebook</button>
+                        <button class="btn-sm btn btn-primary" type="button"><i class="fab fa-instagram text-white pr-3"></i>Instagram</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="map-sec py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="map-area">
+                    <div id="googleMap" style="width:100%;height:500px;"></div>
+                </div>
             </div>
         </div>
     </div>
