888b91487491d7c2a14d755b596c2842ae1ca94f
kidoschool <fauzan.falke@kido.school>
2022-04-11 09:37:31 +0530

    map-updated

--- india/footer.php
+++ india/footer.php
@@ -209,8 +209,8 @@ $('.past-evnts').owlCarousel({
 
     
 </script> 
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIJkPLyzo5XZODdhxAa_XYbk2ESQpJyZw&callback=myMap"></script> -->
-<!-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVtvix8ZhA1BRZLHS_DRSJtFsmQ8FQdf0&callback=myMap"></script> -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCIJkPLyzo5XZODdhxAa_XYbk2ESQpJyZw&callback=myMap"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAVtvix8ZhA1BRZLHS_DRSJtFsmQ8FQdf0&callback=myMap"></script>
 </body>
 
  </html>
